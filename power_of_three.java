6936d097d99cd869e12a8db4a893f577f82a59a8
Benjamin Centner (PC) <bcentner@umich.edu>
2022-08-23 20:36:18 -0400

    kinda work

--- power_of_three.java
+++ power_of_three.java
@@ -3,9 +3,16 @@
 An integer n is a power of three, if there exists an integer x such that n == 3x.
 */
 
+// FIXME: this does not pass all edge cases
 public boolean isPowerOfThree(int n) {
     if (n == 1) {
         return true;
     }
+    while (n >= 3) {
+        if (n == 3) {
+            return true;
+        }
+        n /= 3;
+    }// while 
     return false;
 }
