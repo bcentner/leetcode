088d7322bda4690eebe71539881a6abb846eb30a
Benjamin Centner (PC) <bcentner@umich.edu>
2022-08-25 17:11:36 -0400

    touch

--- palindrome_number.java
+++ palindrome_number.java
@@ -7,5 +7,13 @@ For example, 121 is a palindrome while 123 is not.
  */
 
 public boolean isPalindrome(int x) {
-    String str = String.valueOf(x)''       
+    String str = String.valueOf(x);
+    int rem = 0;
+
+    while (x) {
+        rem = x % 10;
+        
+
+        x /= 10;
+    }// while x not zero
 }
