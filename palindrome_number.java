dbb212074d056228e70358527764a8b0e332c7b1
Benjamin Centner (PC) <bcentner@umich.edu>
2022-08-29 20:04:52 -0400

    slight work

--- palindrome_number.java
+++ palindrome_number.java
@@ -9,11 +9,15 @@ For example, 121 is a palindrome while 123 is not.
 public boolean isPalindrome(int x) {
     String str = String.valueOf(x);
     int rem = 0;
+    int idx = 0;
 
-    while (x) {
+    while (x != 0) {
         rem = x % 10;
-        
+        if (rem != str.charAt(idx)) { //FIXME: these semantics are wrong
+            return false;
+        }
 
         x /= 10;
     }// while x not zero
+    return true;
 }
